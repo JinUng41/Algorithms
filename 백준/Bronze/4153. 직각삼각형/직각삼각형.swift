41496e50050e96b03e9ae73e9d65c90e485dcf07
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-01-31 18:25:54 +0900

    [Bronze III] Title: 직각삼각형, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.swift
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.swift
@@ -2,7 +2,5 @@ while true {
     let abc = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
     let (a, b, c) = (abc[0], abc[1], abc[2])
     if (0, 0, 0) == (a, b, c) { break }
-    let x = (a*a)+(b*b)
-    let y = (c*c)
-    print(x==y ? "right" : "wrong")
+    print((a*a)+(b*b)==(c*c) ? "right" : "wrong")
 }
