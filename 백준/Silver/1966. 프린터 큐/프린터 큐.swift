240a0a379944786b0d296decd36308d6f7588dc2
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-01-31 18:13:03 +0900

    [Silver III] Title: 프린터 큐, Time: 12 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.swift
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.swift
@@ -32,17 +32,13 @@ func solution(_ testCase: Int) {
         var q = Queue<Document>()
         var sort: [Int] = input.sorted(by: { $0 > $1 }).reversed()
         
-        for i in 0..<n {
-            let level = input[i]
-            q.enqueue((i, level))
-        }
+        for i in 0..<n { q.enqueue((i, input[i])) }
         
         while true {
             if q.isEmpty { break }
-            let maxLevel = sort.last!
             let document = q.dequeue()
             
-            if document.level == maxLevel {
+            if document.level == sort.last! {
                 count += 1
                 sort.removeLast()
                 if document.idx == m {
