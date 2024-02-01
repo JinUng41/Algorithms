d49bc4325e6958539de168ea8ecb1587de33cf7d
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-01 14:05:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 756 ms, Memory: 126908 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
@@ -7,28 +7,13 @@ func foo() {
     var dict = [Int: Int]()
     container.forEach { dict[$0] = 0 }
     
-    let sort = container.sorted()
-    var result = ""
-    for i in 0..<n {
-        var left = 0
-        var right = m-1
-        let number = numbers[i]
-        
-        while left<=right {
-            let mid = (left + right) / 2
-            
-            if number == sort[mid] {
-                dict[number]! += 1
-                break
-            }
-            
-            if number < sort[mid] {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
+    numbers.forEach {
+        if let value = dict[$0] {
+            dict[$0]! += 1
         }
     }
+    
+    var result = ""
     container.forEach { result.append("\(dict[$0]!) ") }
     print(result)
 }
