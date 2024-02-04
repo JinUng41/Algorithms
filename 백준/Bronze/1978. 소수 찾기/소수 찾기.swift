208c976837b57a7f90fd503b807c5d601618925d
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-04 21:02:07 +0900

    [Bronze II] Title: 소수 찾기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
@@ -3,13 +3,8 @@ let input = readLine()!.split(separator: " ").map { Int($0)! }
 var count = 0
 func check(_ n: Int) -> Bool {
     if n < 2 { return false }
-    
-    for i in 2..<n {
-        if n % i == 0 { return false }
-    }
-    
+    for i in 2..<n { if n % i == 0 { return false } }
     return true
 }
-
 input.forEach { if check($0) { count += 1 } }
 print(count)
