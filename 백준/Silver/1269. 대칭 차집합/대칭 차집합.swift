c532bc99aa18bc89870cb90d28c4a6f6473bed4a
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-06 11:00:02 +0900

    [Silver IV] Title: 대칭 차집합, Time: 224 ms, Memory: 93644 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.swift
@@ -0,0 +1,4 @@
+let _ = readLine()!.split(separator: " ").map { Int($0)! }
+let a = Set(readLine()!.split(separator: " ").map { Int($0)! })
+let b = Set(readLine()!.split(separator: " ").map { Int($0)! })
+print(a.subtracting(b).count+b.subtracting(a).count)
