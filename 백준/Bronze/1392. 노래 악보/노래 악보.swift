7020131471689acc6d9274b39fa6d051d8abcc44
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-07 20:11:00 +0900

    [Bronze II] Title: 노래 악보, Time: 12 ms, Memory: 69244 KB -BaekjoonHub

--- 백준/Bronze/1392. 노래 악보/노래 악보.swift
+++ 백준/Bronze/1392. 노래 악보/노래 악보.swift
@@ -1,9 +1,6 @@
 let nq = readLine()!.split(separator: " ").map { Int($0)! }
 var songs = [Int]()
-for i in 1...nq[0] {
-    let time = Int(readLine()!)!
-    for _ in 0..<time { songs.append(i) }
-}
+for i in 1...nq[0] { for _ in 0..<Int(readLine()!)! { songs.append(i) } }
 var result = ""
 for _ in 0..<nq[1] { result.append("\(songs[Int(readLine()!)!])\n") }
 print(result)
