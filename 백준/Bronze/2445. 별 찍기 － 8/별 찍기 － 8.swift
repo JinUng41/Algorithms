df10df66696e12263e942f9703a14f4a1ac7f165
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-08 11:29:50 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.swift
@@ -0,0 +1,15 @@
+let n = Int(readLine()!)!
+var result = ""
+for i in 1...n {
+    let a = String(repeating: "*", count: i)
+    let b = String(repeating: " ", count: n*2-i*2)
+    result.append("\(a+b+a)\n")
+}
+if  n > 1 {
+    for i in (1...(n-1)).reversed() {// Thread 1: Fatal error: Range requires lowerBound <= upperBound
+        let a = String(repeating: "*", count: i)
+        let b = String(repeating: " ", count: n*2-i*2)
+        result.append("\(a+b+a)\n")
+    }
+}
+print(result)
