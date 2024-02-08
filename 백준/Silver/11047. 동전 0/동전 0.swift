0710755ae9eb7f1029181c1ef11a0872d8d3aa73
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-08 13:32:14 +0900

    [Silver IV] Title: 동전 0, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.swift
+++ 백준/Silver/11047. 동전 0/동전 0.swift
@@ -3,9 +3,8 @@ var n = nk[0], k = nk[1]
 var coins = [Int]()
 for _ in 0..<n { coins.append(Int(readLine()!)!) }
 var count = 0
-while k != 0 {
-    let last = coins.removeLast()
-    count += k / last
-    k = k % last
+for coin in coins.reversed() {
+    count += k / coin
+    k = k % coin
 }
 print(count)
