432ee582d34559eff9fde7439b12613e98f6b262
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-20 03:41:15 +0900

    [Silver II] Title: 풍선 공장, Time: 496 ms, Memory: 134296 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.swift
@@ -0,0 +1,16 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+let times = readLine()!.split(separator: " ").map { Int($0)! }
+var left = 0
+var right = m * times.max()!
+while left <= right {
+    let mid = (left + right) / 2
+    var balloons = 0
+    for time in times { balloons += mid / time }
+    if balloons >= m {
+        right = mid - 1
+    } else {
+        left = mid + 1
+    }
+}
+print(left)
