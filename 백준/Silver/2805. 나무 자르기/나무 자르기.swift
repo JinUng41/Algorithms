6d2ac330be1d6f7bd018cf60c60b37d432fe3c0b
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-20 19:01:26 +0900

    [Silver II] Title: 나무 자르기, Time: 568 ms, Memory: 145408 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -1,5 +1,4 @@
 let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0], m = nm[1]
 let trees = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
 var left = 0
 var right = trees.last!
@@ -7,7 +6,7 @@ while left <= right {
     let mid = (left + right) / 2
     var meter = 0
     for tree in trees { if tree > mid { meter += tree - mid } }
-    if meter >= m {
+    if meter >= nm[1] {
         left = mid + 1
     } else {
         right = mid - 1
