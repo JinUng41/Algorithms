b6a15948f510b55de430c99008d0af96d3b93cb2
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-23 05:54:15 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 400 ms, Memory: 82756 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.swift
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.swift
@@ -1,31 +1,23 @@
 struct Queue<T> {
     private var input = [T]()
     private var output = [T]()
-    
-    var isEmpty: Bool {
-        return input.isEmpty && output.isEmpty
-    }
-    
-    mutating func enq(_ new: T) {
-        input.append(new)
-    }
-    
+    var isEmpty: Bool { return input.isEmpty && output.isEmpty }
+    mutating func enq(_ new: T) { input.append(new) }
     mutating func deq() -> T {
         if output.isEmpty {
             output = input.reversed()
             input.removeAll()
         }
-        
         return output.removeLast()
     }
 }
 let nmr = readLine()!.split(separator: " ").map { Int($0)! }
 let n = nmr[0], m = nmr[1], r = nmr[2]
-var isVisit = [Bool](repeating: false, count: n+1)
 var visitors = [Int](repeating: 0, count: n+1)
 var graph = [[Int]](repeating: [], count: n+1)
-var count = 1
 func bfs(_ node: Int) {
+    var isVisit = [Bool](repeating: false, count: n+1)
+    var count = 1
     isVisit[node] = true
     visitors[node] = count
     var q = Queue<Int>()
