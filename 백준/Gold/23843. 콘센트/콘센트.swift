7db8b95b30ab788c064191e149aa05c7960f1430
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-23 07:05:24 +0900

    [Gold V] Title: 콘센트, Time: 12 ms, Memory: 69536 KB -BaekjoonHub

---
+++ 백준/Gold/23843. 콘센트/콘센트.swift
@@ -0,0 +1,59 @@
+struct Heap<T> {
+    private var array = [T]()
+    private let compare: (T, T) -> Bool
+    var count: Int { array.count }
+    var isEmpty: Bool { array.isEmpty }
+    var peek: T? { array.first }
+    init(by compare: @escaping (T, T) -> Bool) { self.compare = compare }
+    
+    mutating func enheap(_ newElement: T) {
+        var childIndex = array.count
+        var parentIndex = (childIndex - 1) / 2
+        array.append(newElement)
+        
+        while childIndex > 0 && compare(array[childIndex], array[parentIndex]) {
+            array.swapAt(childIndex, parentIndex)
+            childIndex = parentIndex
+            parentIndex = (childIndex - 1) / 2
+        }
+    }
+    
+    mutating func deheap() -> T? {
+        if array.count == 1 { return array.removeLast() }
+        
+        let value = array[0]
+        array[0] = array.removeLast()
+        
+        var index = 0
+        while true {
+            let leftChildIndex = index * 2 + 1
+            let rightChildIndex = leftChildIndex + 1
+            
+            var target = index
+            if leftChildIndex < array.count && compare(array[leftChildIndex], array[target]) { target = leftChildIndex }
+            
+            if rightChildIndex < array.count && compare(array[rightChildIndex], array[target]) { target = rightChildIndex }
+            
+            if target == index { break }
+            
+            array.swapAt(target, index)
+            index = target
+        }
+        return value
+    }
+}
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+let devices = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
+if m < n {
+    var charging = Heap<Int>(by: <)
+    for i in 0..<m { charging.enheap(devices[i]) }
+    for i in m..<n {
+        let minCharging = charging.deheap()! // 가장 적게 충전된 기기의 충전 시간
+        charging.enheap(minCharging + devices[i]) // 다음 기기를 충전
+    }
+    while charging.count != 1 { let _ = charging.deheap() }
+    print(charging.peek!)
+} else {
+    print(devices.first!)
+}
