cdf760b625069246ee1dd1f39a57a521856c3591
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-23 21:59:44 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 12 ms, Memory: 69280 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
@@ -0,0 +1,10 @@
+let nk = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nk[0], k = nk[1]
+var a = Array(1...n), b = [Int]() 
+var i = 0
+while a.count > 0 {
+    i = (i + k - 1) % a.count
+    b.append(a.remove(at: i))
+}
+let z = "<" + b.map { "\($0)" }.joined(separator: ", ") + ">"
+print(z)
