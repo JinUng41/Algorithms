8f69fa912eb0162e5825886d2b43308f709350b9
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-02-23 22:05:27 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 12 ms, Memory: 69280 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
@@ -2,7 +2,7 @@ let nk = readLine()!.split(separator: " ").map { Int($0)! }
 let n = nk[0], k = nk[1]
 var a = Array(1...n), b = [Int]() 
 var i = 0
-while a.count > 0 {
+while !a.isEmpty {
     i = (i + k - 1) % a.count
     b.append(a.remove(at: i))
 }
