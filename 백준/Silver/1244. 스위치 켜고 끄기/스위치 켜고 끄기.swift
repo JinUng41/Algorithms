317930fde97d9fd53b4af4b50bb164ed751a144f
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-04 18:41:30 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 16 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.swift
@@ -0,0 +1,36 @@
+func BOJ_1244() {
+    func toggle(_ plug: Int) { status[plug] = status[plug] == 1 ? 0 : 1 }
+    
+    let plugNumber = Int(readLine()!)!
+    var status = [-1] + readLine()!.split(separator: " ").map { Int($0)! }
+    let studentNumber = Int(readLine()!)!
+    
+    for _ in 0..<studentNumber {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        let gender = input[0], plug = input[1]
+        
+        if gender == 1 {
+            for i in stride(from: plug, through: plugNumber, by: plug) { toggle(i) }
+        } else {
+            toggle(plug)
+            for i in 0..<(plugNumber) {
+                let a = plug + i, b = plug - i
+                
+                if a > plugNumber || b < 1 { break }
+                
+                if status[a] == status[b] {
+                    toggle(a)
+                    toggle(b)
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    
+    for i in 1...plugNumber {
+        print("\(status[i])", terminator: " ")
+        if i % 20 == 0 { print() }
+    }
+}
+BOJ_1244()
