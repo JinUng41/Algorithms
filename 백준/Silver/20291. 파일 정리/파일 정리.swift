b189b0dce78906f18cfc956998e37d7081774305
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-08 19:56:34 +0900

    [Silver III] Title: 파일 정리, Time: 344 ms, Memory: 91984 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.swift
+++ 백준/Silver/20291. 파일 정리/파일 정리.swift
@@ -1,14 +1,13 @@
 func BOJ_20291() {
     let N = Int(readLine()!)!
-    var extensions = [String: Int]()
+    var files = [String: Int]()
     for _ in 0..<N {
         let input = readLine()!.split(separator: ".").map { String($0) }
-        if extensions[input[1]] == nil { extensions[input[1]] = 1 }
-        else { extensions[input[1]]! += 1 }
+        if files[input[1]] == nil { files[input[1]] = 1 }
+        else { files[input[1]]! += 1 }
     }
-    let temp = extensions.sorted { $0.key < $1.key }
     var z = ""
-    for file in temp { z += "\(file.key) \(file.value)\n" }
+    for file in files.sorted(by: { $0.key < $1.key }) { z += "\(file.key) \(file.value)\n" }
     print(z)
 }
 BOJ_20291()
