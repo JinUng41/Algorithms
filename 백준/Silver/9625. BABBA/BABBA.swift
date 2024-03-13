10aaa9fa2e3b867ec8b0bc1df6fbb2d6164d3c4d
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-14 07:08:11 +0900

    [Silver V] Title: BABBA, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.swift
+++ 백준/Silver/9625. BABBA/BABBA.swift
@@ -1,16 +1,11 @@
 func BOJ_9625() {
     let K = Int(readLine()!)!
-    var a = [Int](repeating: 0, count: 46), b = [Int](repeating: 0, count: 46)
-    a[0] = 1
-    a[2] = 1
-    b[1] = 1
-    b[2] = 1
-    if K > 2 {
-        for i in 3...K {
-            a[i] = a[i - 1] + a[i - 2]
-            b[i] = b[i - 1] + b[i - 2]
-        }
+    var a = 1, b = 0
+    for _ in 1...K {
+        let c = b
+        b += a
+        a = c
     }
-    print("\(a[K]) \(b[K])")
+    print("\(a) \(b)")
 }
 BOJ_9625()
