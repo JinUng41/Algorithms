0c66a173dd93ddfdf85647e4728116980d82c6ea
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-16 00:00:52 +0900

    [Silver IV] Title: 암기왕, Time: 1648 ms, Memory: 159512 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.swift
@@ -0,0 +1,24 @@
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let N = Int(readLine()!)!
+    var nArr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+    let M = Int(readLine()!)!
+    var mArr = readLine()!.split(separator: " ").map { Int($0)! }
+    var result = Array(repeating: 0, count: M)
+    for i in 0..<M {
+        var left = 0
+        var right = N - 1
+        while left <= right {
+            let mid = (left + right) / 2
+            if mArr[i] < nArr[mid] {
+                right = mid - 1
+            } else if mArr[i] > nArr[mid] {
+                left = mid + 1
+            } else {
+                result[i] = 1
+                break
+            }
+        }
+    }
+    print(result.map({ "\($0)" }).joined(separator: "\n"))
+}
