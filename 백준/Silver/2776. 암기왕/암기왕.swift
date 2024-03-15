9a5d952d14d0f19ab46056ee589922d7263c7329
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-16 00:04:09 +0900

    [Silver IV] Title: 암기왕, Time: 1592 ms, Memory: 159512 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.swift
+++ 백준/Silver/2776. 암기왕/암기왕.swift
@@ -1,24 +1,21 @@
-let T = Int(readLine()!)!
-for _ in 0..<T {
-    let N = Int(readLine()!)!
-    var nArr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-    let M = Int(readLine()!)!
-    var mArr = readLine()!.split(separator: " ").map { Int($0)! }
-    var result = Array(repeating: 0, count: M)
-    for i in 0..<M {
-        var left = 0
-        var right = N - 1
-        while left <= right {
-            let mid = (left + right) / 2
-            if mArr[i] < nArr[mid] {
-                right = mid - 1
-            } else if mArr[i] > nArr[mid] {
-                left = mid + 1
-            } else {
-                result[i] = 1
-                break
+func BOJ_2776() {
+    for _ in 0..<Int(readLine()!)! {
+        let N = Int(readLine()!)!
+        let nArr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+        let M = Int(readLine()!)!
+        let mArr = readLine()!.split(separator: " ").map { Int($0)! }
+        var result = Array(repeating: 0, count: M)
+        for i in 0..<M {
+            var left = 0
+            var right = N - 1
+            while left <= right {
+                let mid = (left + right) / 2
+                if mArr[i] < nArr[mid] { right = mid - 1 }
+                else if mArr[i] > nArr[mid] { left = mid + 1 }
+                else { result[i] = 1; break }
             }
         }
+        print(result.map({ "\($0)" }).joined(separator: "\n"))
     }
-    print(result.map({ "\($0)" }).joined(separator: "\n"))
 }
+BOJ_2776()
