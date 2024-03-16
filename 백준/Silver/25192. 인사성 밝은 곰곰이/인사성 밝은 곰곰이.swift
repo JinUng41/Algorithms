7a586a6d423c83d566006d99741136d5388ecf78
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-16 18:10:03 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 88 ms, Memory: 76036 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.swift
@@ -0,0 +1,18 @@
+func BOJ_25192() {
+    let N = Int(readLine()!)!
+    var a = Set<String>()
+    var b = 0
+    for _ in 0..<N {
+        let input = readLine()!
+        if input == "ENTER" {
+            a.removeAll()
+            continue
+        }
+        if !a.contains(input) {
+            a.insert(input)
+            b += 1
+        }
+    }
+    print(b)
+}
+BOJ_25192()
