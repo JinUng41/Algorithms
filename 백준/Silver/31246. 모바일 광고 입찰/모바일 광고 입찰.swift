5b3ae8b493210da02dfbdd8e5c70297a9f3c4284
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-19 08:13:51 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 176 ms, Memory: 71328 KB -BaekjoonHub

--- 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.swift
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.swift
@@ -1,20 +1,18 @@
 func BOJ_31246() {
     let NK = readLine()!.split(separator: " ").map { Int($0)! }
-    let N = NK[0], K = NK[1]
-    var count = K
+    let N = NK[0]
+    var K = NK[1]
     var diff = [Int]()
     for _ in 0..<N {
         let AB = readLine()!.split(separator: " ").map { Int($0)! }
-        let A = AB[0], B = AB[1]
-        let n = B-A
-        if n <= 0 { count -= 1 }
-        else { diff.append(B-A) }
+        let n = AB[1] - AB[0]
+        if n <= 0 { K -= 1 }
+        else { diff.append(n) }
     }
-    if count <= 0 { print(0); return }
-    
+    if K <= 0 { print(0); return }
     for n in diff.sorted() {
-        count -= 1
-        if count == 0 { print(n); return }
+        K -= 1
+        if K == 0 { print(n); return }
     }
 }
 BOJ_31246()
