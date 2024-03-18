37ba1e36ec1202ca5a8f019bb446841f9da8cdf3
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-19 08:41:31 +0900

    [Silver V] Title: 단어 정렬, Time: 464 ms, Memory: 74408 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.swift
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.swift
@@ -1,7 +1,5 @@
-var array = [String]()
-for _ in 0..<Int(readLine()!)! {
-    array.append(readLine()!)
-}
-var temp = Array(Set(array))
-temp.sort { $0.count == $1.count ? $0 < $1 : $0.count < $1.count }
-temp.forEach { print($0) }
+var set = Set<String>()
+for _ in 0..<Int(readLine()!)! { set.insert(readLine()!) }
+var z = ""
+set.sorted { $0.count == $1.count ? $0 < $1 : $0.count < $1.count }.forEach { z += "\($0)\n"}
+print(z)
