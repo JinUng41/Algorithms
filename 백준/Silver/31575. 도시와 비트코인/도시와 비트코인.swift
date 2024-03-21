efa6e1fc679c13695472f4542dc757c125ef7380
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-21 15:34:42 +0900

    [Silver III] Title: 도시와 비트코인, Time: 28 ms, Memory: 69860 KB -BaekjoonHub

---
+++ 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.swift
@@ -0,0 +1,19 @@
+func BOJ_31575() {
+    let NM = readLine()!.split(separator: " ").map { Int($0)! }
+    let N = NM[0], M = NM[1]
+    let dx = [0, 1], dy = [1, 0]
+    var map = [[Int]]()
+    var isVisit = Array(repeating: Array(repeating: false, count: N), count: M)
+    for _ in 0..<M { map.append(readLine()!.split(separator: " ").map { Int($0)! }) }
+    func check(_ x: Int, _ y: Int) -> Bool { return x >= 0 && x < N && y >= 0 && y < M && map[y][x] == 1 }
+    func dfs(_ x: Int, _ y: Int) {
+        isVisit[y][x] = true
+        for i in 0..<2 {
+            let newX = x + dx[i], newY = y + dy[i]
+            if check(newX, newY), !isVisit[newY][newX] { dfs(newX, newY) }
+        }
+    }
+    dfs(0, 0)
+    print(isVisit[M - 1][N - 1] ? "Yes" : "No")
+}
+BOJ_31575()
