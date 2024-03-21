1af56d8d39ba3f2263f78b97d222bca63abdea7f
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-22 01:16:50 +0900

    [Silver IV] Title: 수 찾기, Time: 136 ms, Memory: 81396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,6 +1,6 @@
 let n = Int(readLine()!)!
 var nSet = Set(readLine()!.split(separator: " ").map { Int($0)! })
-let m = Int(readLine()!)!
+_ = readLine()
 var result = ""
 readLine()!.split(separator: " ").map { Int($0)! }.forEach {
     result.append(nSet.contains($0) ? "1\n" : "0\n")
