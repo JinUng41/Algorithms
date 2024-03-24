637f4629fee0a994e638fc35a3280d56cdeee764
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-03-24 14:29:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.swift
@@ -0,0 +1,15 @@
+func BOJ_1003(_ T: Int) {
+    var numbers = [Int]()
+    for _ in 0..<T { 
+        numbers.append(Int(readLine()!)!)
+    }
+    let max = numbers.max()!
+    var dp = Array(repeating: (0, 0), count: 41)
+    dp[0] = (1, 0)
+    dp[1] = (0, 1)
+    for i in 2...40 { dp[i] = (dp[i-1].1, dp[i-1].1 + dp[i-2].1) }
+    var z = ""
+    numbers.forEach { z += "\(dp[$0].0) \(dp[$0].1)\n" }
+    print(z)
+}
+BOJ_1003(Int(readLine()!)!)
