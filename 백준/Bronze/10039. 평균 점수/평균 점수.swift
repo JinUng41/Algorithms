ad4d5e9dbfb010c7fb64059f2adf13eb0f92193f
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-05-02 18:03:08 +0900

    [Bronze IV] Title: 평균 점수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.swift
+++ 백준/Bronze/10039. 평균 점수/평균 점수.swift
@@ -1,10 +1,9 @@
 func BOJ_10039() {
-    var scores = [Int]()
+    var sum = 0
     for _ in 0..<5 {
         let score = Int(readLine()!)!
-        scores.append(score < 40 ? 40 : score)
+        sum += score < 40 ? 40 : score
     }
-    let total = scores.reduce(0,+)
-    print(total/5)
+    print(sum/5)
 }
 BOJ_10039()
