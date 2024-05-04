b83c8fec609fceda0221cbde43250daa500868d8
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-05-04 17:34:47 +0900

    [Silver V] Title: 매우 어려운 문제, Time: 100 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.swift
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.swift
@@ -3,11 +3,8 @@ func BOJ_31738(_ N: Int, _ M: Int) {
         print(0)
         return
     }
-    
     var result = 1
-    for i in 1...N {
-        result = (result * i) % M
-    }
+    for i in 1...N { result = (result * i) % M }
     print(result)
 }
 let NM = readLine()!.split(separator:  " ").map { Int($0)! }
