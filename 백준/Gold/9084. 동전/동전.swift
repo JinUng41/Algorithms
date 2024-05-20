211b25c8cf07c86b8fbbb84ff10e3c32845ddbaa
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-05-20 11:16:14 +0900

    [Gold V] Title: 동전, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.swift
@@ -0,0 +1,14 @@
+func BOJ_9084(_ N: Int) {
+    let coins = readLine()!.split(separator: " ").map { Int($0)! }
+    let m = Int(readLine()!)!
+    var dp = Array(repeating: 0, count: m+1)
+    dp[0] = 1
+    for coin in coins {
+        if coin > m { continue }
+        for won in coin...m {
+            dp[won] += dp[won-coin]
+        }
+    }
+    print(dp[m])
+}
+for _ in 0..<Int(readLine()!)! { BOJ_9084(Int(readLine()!)!) }
