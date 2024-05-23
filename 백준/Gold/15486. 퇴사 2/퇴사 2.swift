409d18d85bca68f2870f104bdc2d09846b9eda91
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-05-23 10:22:44 +0900

    [Gold V] Title: 퇴사 2, Time: 1584 ms, Memory: 80824 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -0,0 +1,13 @@
+func BOJ_15486(_ N: Int) {
+    var dp = Array(repeating: 0, count: N+2)
+    for i in 1...N {
+        let tp = readLine()!.split(separator: " ").map { Int($0)! }
+        let t = tp[0], p = tp[1]
+        
+        if dp[i] > dp[i+1] { dp[i+1] = dp[i] }
+        if i+tp[0] > N+1 { continue }
+        dp[i+t] = max(p + dp[i], dp[i+t])
+    }
+    print(dp[N+1])
+}
+BOJ_15486(Int(readLine()!)!)
