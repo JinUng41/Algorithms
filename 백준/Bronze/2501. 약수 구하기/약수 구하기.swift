07c9c6cfe8c6c4fa0a06d5bfa587b675a8b62b02
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-06-04 08:51:41 +0900

    [Bronze III] Title: 약수 구하기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.swift
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.swift
@@ -3,9 +3,7 @@ BOJ_2501(NK[0], NK[1]-1)
 func BOJ_2501(_ N: Int, _ K: Int) {
     var numbers = [Int]()
     for i in 1...N {
-        if N % i == 0 {
-            numbers.append(i)
-        }
+        if N % i == 0 { numbers.append(i) }
     }
     print(numbers.count > K ? numbers[K] : 0)
 }
