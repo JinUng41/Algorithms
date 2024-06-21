c5d833d2781f3d60d3e9f202ae64bb106f59950b
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-06-21 13:03:23 +0900

    [Silver IV] Title: 사탕, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1812. 사탕/사탕.swift
+++ 백준/Silver/1812. 사탕/사탕.swift
@@ -1,16 +1,10 @@
 func BOJ_1812(_ N: Int) {
-    var sums = [Int](), candies = [Int]()
+    var sums = [Int](), candies = [Int](repeating: 0, count: N)
     for _ in 0..<N { sums.append(Int(readLine()!)!) }
     var first = 0
-    for (index, sum) in sums.enumerated() {
-        if index % 2 == 0 {
-            first += sum
-        } else {
-            first -= sum
-        }
-    }
-    candies.append(first/2)
-    for i in 0..<N-1 { candies.append(sums[i]-candies[i]) }
-    for candy in candies { print(candy) }
+    for (index, sum) in sums.enumerated() { first += (index % 2 == 0 ? sum : -sum) }
+    candies[0] = first/2
+    for i in 1..<N { candies[i] = sums[i-1]-candies[i-1] }
+    print(candies.map { String($0) }.joined(separator: "\n"))
 }
 BOJ_1812(Int(readLine()!)!)
