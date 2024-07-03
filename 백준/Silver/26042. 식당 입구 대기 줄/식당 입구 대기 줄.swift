7440b6c2c1427b09dd5184f977f65bca809a8630
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-07-03 22:01:22 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 100 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.swift
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.swift
@@ -11,24 +11,24 @@ struct Queue<T> {
         return output.removeLast()
     }
 }
-func BOJ_3986(_ n: Int) {
+func BOJ_26042(_ n: Int) {
     var queue = Queue<Int>()
-    var maxCount = 0, lastStudent = 0
+    var count = 0, temp = 0
     for _ in 0..<n {
         let input = readLine()!.split(separator: " ")
         if input[0] == "1" {
             let student = Int(input[1])!
             queue.enq(student)
-            if queue.count > maxCount {
-                maxCount = queue.count
-                lastStudent = student
-            } else if queue.count == maxCount {
-                lastStudent = min(lastStudent, student)
+            if queue.count > count {
+                count = queue.count
+                temp = student
+            } else if queue.count == count {
+                temp = min(temp, student)
             }
         } else {
             _ = queue.deq()
         }
     }
-    print(maxCount, lastStudent)
+    print(count, temp)
 }
-BOJ_3986(Int(readLine()!)!)
+BOJ_26042(Int(readLine()!)!)
