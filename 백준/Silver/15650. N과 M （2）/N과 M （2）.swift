f312a3a90fb0005d43b77dc2ae8bd51e559d7c84
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-07-31 14:05:40 +0900

    [Silver III] Title: N과 M (2), Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -1,14 +1,11 @@
 func BOJ_15650(_ N: Int, _ M: Int) {
     var arr = [Int]()
-    
     func backTracking(_ depth: Int) {
         if arr.count == M {
             print(arr.map { "\($0)" }.joined(separator: " "))
             return
         }
-        
         if depth > N { return }
-        
         for i in depth...N {
             arr.append(i)
             backTracking(i+1)
