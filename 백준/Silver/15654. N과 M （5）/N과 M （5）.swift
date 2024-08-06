7af4bedfd978f33f4287c8781cd7d6442887ffa2
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-08-06 09:00:21 +0900

    [Silver III] Title: N과 M (5), Time: 88 ms, Memory: 73056 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
@@ -4,20 +4,18 @@ func BOJ_15654(_ N: Int, _ M: Int) {
     let input = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
     var arr = [Int](), isVisit = Array(repeating: false, count: N+1)
     var result = ""
-    
     func backTracking() {
         if arr.count == M {
             result += "\(arr.map { "\($0)" }.joined(separator: " "))\n"
             return
         }
-        
         for i in 1...N {
             if !isVisit[i] {
                 arr.append(input[i-1])
                 isVisit[i] = true
                 backTracking()
                 isVisit[i] = false
-                arr.removeLast()
+                _ = arr.popLast()
             }
         }
     }
