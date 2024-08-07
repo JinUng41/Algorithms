cb9127708ae0ab2d6721e9f801544cc7438fbed1
지눙 <91656206+JinUng41@users.noreply.github.com>
2024-08-07 11:43:09 +0900

    [Silver III] Title: N과 M (1), Time: 68 ms, Memory: 70100 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -1,23 +1,21 @@
 func BOJ_15649(_ N: Int, _ M: Int) {
-    var arr = [Int](), isVisit = Array(repeating: false, count: N+1)
-    
-    func backTracking() {
-        if arr.count == M {
-            print(arr.map { "\($0)" }.joined(separator: " "))
+    var arr = [String](), isVisit = Array(repeating: false, count: N+1)
+    var result = ""
+    func foo(_ depth: Int) {
+        if depth == M {
+            result += "\(arr.joined(separator: " "))\n"
             return
         }
-        
-        for i in 1...N {
-            if !isVisit[i] {
-                isVisit[i] = true
-                arr.append(i)
-                backTracking()
-                arr.removeLast()
-                isVisit[i] = false
-            }
+        for i in 1...N where !isVisit[i] {
+            isVisit[i] = true
+            arr.append("\(i)")
+            foo(depth+1)
+            arr.removeLast()
+            isVisit[i] = false
         }
     }
-    backTracking()
+    foo(0)
+    print(result)
 }
 let NM = readLine()!.split(separator: " ").map { Int($0)! }
 BOJ_15649(NM[0], NM[1])
