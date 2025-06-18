1d803309e7cd31a2c58e930b1297db10968a415d
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-18 09:50:31 +0900

    [level 0] Title: 할 일 목록, Time: 0.17 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.swift
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ todo_list:[String], _ finished:[Bool]) -> [String] {
-    return zip(todo_list, finished).compactMap { $1 ? nil : $0 }
+    return zip(todo_list, finished).filter { !$0.1 }.map { $0.0 }
 }
