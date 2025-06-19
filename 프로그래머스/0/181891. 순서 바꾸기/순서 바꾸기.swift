b7495608110e4f86f1789248ab59ebb1ff58bca6
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-19 09:36:27 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.11 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.swift
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    var temp = [Int]()
-    for i in n..<num_list.count {
-        temp.append(num_list[i])
-    }
-    for i in 0..<n {
-        temp.append(num_list[i])
-    }
-    return temp
+    return (Array(num_list[n...]) + Array(num_list[0..<n]))
 }
