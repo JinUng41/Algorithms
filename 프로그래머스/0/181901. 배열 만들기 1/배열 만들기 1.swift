960d422c4c2cadb036fa301209fe0556ea0babd0
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-19 09:53:27 +0900

    [level 0] Title: 배열 만들기 1, Time: 471.32 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.swift
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ n:Int, _ k:Int) -> [Int] {
-    var temp = [Int]()
-    for i in 1...n where i % k == 0 {
-        temp.append(i)
-    }
-    return temp
+    return (1...n).filter { $0 % k == 0 }
 }
