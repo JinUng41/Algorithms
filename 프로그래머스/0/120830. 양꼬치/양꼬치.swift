fd222a03f7b2221379cb42ac42ab07c2c27bb476
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-21 08:58:52 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ n:Int, _ k:Int) -> Int {
-    var price = 12000 * n + 2000 * k
-    if n >= 10 {
-        var num = n / 10
-        price = price - 2000 * num
-    }
-    return price
+    return 12000 * n + 2000 * (k - n / 10)
 }
