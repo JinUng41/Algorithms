56deaf52c79e9a2804234078ad62b185f057cf99
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-21 08:45:02 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 7.34 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.swift
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.swift
@@ -3,7 +3,6 @@ import Foundation
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 var result = ""
 for i in 1...n[0] {
-    for _ in 1...i { result += "*" }
-    result += "\n"
+    result += String(repeating: "*", count: i) + "\n"
 }
 print(result)
