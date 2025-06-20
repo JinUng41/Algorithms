6879420ae49289afc8c28e9fe865550d328255ba
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-20 17:14:52 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 29.63 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
@@ -1,15 +1,8 @@
 import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
-    var result = [Int]()
-    for query in queries {
-        let (s, e, k) = (query[0], query[1], query[2])
-        var minNum = 1_000_001
-        for i in s...e where arr[i] > k {
-            minNum = min(minNum, arr[i])
-        }
-        if minNum == 1_000_001 { minNum = -1}
-        result.append(minNum)
+    return queries.map {
+        let (s, e, k) = ($0[0], $0[1], $0[2])
+        return Array(arr[s...e]).filter { $0 > k }.min() ?? -1
     }
-    return result
 }
