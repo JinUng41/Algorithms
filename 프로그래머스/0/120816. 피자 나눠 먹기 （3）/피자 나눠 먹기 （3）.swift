6fbef3446b5dc07d9dec3728d1d329e08f82e676
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-20 17:22:40 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.swift
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ slice:Int, _ n:Int) -> Int {
-    var sum = 0
-    while sum < n { sum += slice }
-    return sum / slice
+    return n % slice == 0 ? n / slice : n / slice + 1
 }
