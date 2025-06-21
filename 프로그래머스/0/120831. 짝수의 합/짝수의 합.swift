d4ca9b8559b7f91c8d6c33a658572d6b851c91aa
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-21 09:02:08 +0900

    [level 0] Title: 짝수의 합, Time: 0.62 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ n: Int) -> Int {
-    var sum = 0
-    for i in 1...n where i % 2 == 0 { sum += i }
-    return sum
+    return (0...n).filter { $0 % 2 == 0 }.reduce(0, +)
 }
