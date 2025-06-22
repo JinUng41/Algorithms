2008ed90a2d2c6d010fc8eb136b33656567ca124
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-06-22 16:26:46 +0900

    [level 0] Title: 순서쌍의 개수, Time: 500.82 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.swift
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var arr = [(Int, Int)]()
-    for i in 1...n where n % i == 0 {
-        arr.append((i, n / i))
-    }
-    return arr.count
+    return (1...n).filter { n % $0 == 0 }.count
 }
