eda31c25aa5a7b946c0f19669c3763d55f14741c
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-07-02 10:02:49 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.49 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.swift
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ arr:[Int], _ intervals:[[Int]]) -> [Int] {
-    var result = [Int]()
-    for interval in intervals {
-        result.append(contentsOf: Array(arr[interval[0]...interval[1]]))
-    }
-    return result
+    return intervals.flatMap { Array(arr[$0[0]...$0[1]]) }
 }
