8845323bbdcc5963539a928043b1030a103cd327
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-07-02 10:39:38 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.swift
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.swift
@@ -1,17 +1,10 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var factorial = 1
-    var i = 1
-    
-    while true {
-        let nextFactorial = factorial * (i + 1)
-        
-        if nextFactorial > n { break }
-        
-        factorial = nextFactorial
+    var factorial = 1, i = 1
+    while factorial <= n {
+        factorial *= (i + 1)
         i += 1
     }
-    
-    return i
+    return i - 1
 }
