a124a62caf7d92a16eccd75c48c7859302644a97
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-07-05 23:57:08 +0900

    [level 0] Title: 369게임, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.swift
+++ 프로그래머스/0/120891. 369게임/369게임.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ order:Int) -> Int {
-    let numbers: Set<Character> = ["3", "6", "9"]
-    return Array(String(order)).filter { numbers.contains($0) }.count
+    return "\(order)".filter { "369".contains($0) }.count
 }
