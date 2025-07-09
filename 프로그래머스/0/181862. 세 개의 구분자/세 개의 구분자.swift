15a05d68843c511beb58d53dc083a6153f5b81f9
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-07-09 09:32:22 +0900

    [level 0] Title: 세 개의 구분자, Time: 155.25 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.swift
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func solution(_ myStr:String) -> [String] {
-    let result = myStr.components(separatedBy: ["a", "b", "c"]).filter { !$0.isEmpty }
+    let result = myStr.split { $0 == "a" || $0 == "b" || $0 == "c" }.map(String.init)
     return result.isEmpty ? ["EMPTY"] : result
 }
