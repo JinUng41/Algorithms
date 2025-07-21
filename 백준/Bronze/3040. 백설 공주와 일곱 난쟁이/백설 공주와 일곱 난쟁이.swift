a2b43e18546736215536e8c7317c08d38468661e
지눙 <91656206+JinUng41@users.noreply.github.com>
2025-07-21 13:07:19 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.swift
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 let input = (0..<9).map { _ in Int(readLine()!)! }
-let sum = input.reduce(0, +)
+let diff = input.reduce(0, +) - 100
 
 var fake1 = 0, fake2 = 0
 
 loop: for i in 0..<input.count {
-    for j in i + 1..<input.count where sum - (input[i] + input[j]) == 100 {
+    for j in i + 1..<input.count where input[i] + input[j] == diff {
         fake1 = input[i]
         fake2 = input[j]
         break loop
