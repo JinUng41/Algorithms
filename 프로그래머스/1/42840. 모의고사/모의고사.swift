c7380dbdb597e38ef2635c9491312c6ac229ff77
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2025-10-28 08:08:42 +0900

    [level 1] Title: 모의고사, Time: 1.72 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    let patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    
+    var scores = Array(repeating: 0, count: 3)
+    for i in 0..<answers.count {
+        for j in 0..<patterns.count {
+            if answers[i] == patterns[j][i % patterns[j].count] {
+                scores[j] += 1
+            }
+        }
+    }
+    
+    let maxScore = scores.max()
+    return scores.enumerated().filter { $0.element == maxScore ?? .zero }.map { $0.offset + 1 }
+}
