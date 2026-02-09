aaa115212abd78f738f714fa3f51167d452186bf
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-02-09 19:00:13 +0900

    [level 1] Title: 모의고사, Time: 6.66 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -4,17 +4,23 @@ def solution(answers):
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
-    
-    result = [0 for _ in range(len(patterns))]
+
+    patternA = [1, 2, 3, 4, 5]
+    patternB = [2, 1, 2, 3, 2, 4, 2, 5]
+    patternC = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    score = [0, 0, 0]
     for i in range(len(answers)):
-        for j in range(len(patterns)):
-            if answers[i] == patterns[j][i % len(patterns[j])]:
-                result[j] += 1
+        if answers[i] == patternA[i % len(patternA)]:
+            score[0] += 1
+        if answers[i] == patternB[i % len(patternB)]:
+            score[1] += 1
+        if answers[i] == patternC[i % len(patternC)]:
+            score[2] += 1
 
-
-    max_score = max(result)
-    answer = []
-    for i in range(len(patterns)):
-        if result[i] == max_score:
-            answer.append(i + 1)
-    return answer
+    max_score = max(score)
+    result = []
+    for idx, val in enumerate(score):
+        if val == max_score:
+            result.append(idx + 1)
+    return result
