f8797f07d694c0e61498b84b6f436b42799b2914
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-02-19 18:28:12 +0900

    [Silver V] Title: 거스름돈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -2,11 +2,5 @@ n = int(input())
 count = 0
 while n > 1:
     count += 1
-    if n % 5 == 0:
-        n -= 5
-        continue
-    n -= 2
-if n == 0:
-    print(count)
-else:
-    print(-1)
+    n -= 5 if n % 5 == 0 else 2
+print(count if n == 0 else -1)
