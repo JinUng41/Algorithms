c514305e82c5a4bb5527623b3dae33d86af1d21a
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-02-21 13:07:54 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,12 +1,10 @@
 n, k = map(int, input().split())
-divs = []
+divs = set()
 i = 1
 while i * i <= n:
     if n % i == 0:
-        divs.append(i)
-        j = n // i
-        if j != i:
-            divs.append(j)
+        divs.add(i)
+        divs.add(n // i)
     i += 1
-divs.sort()
+divs = sorted(divs)
 print(divs[k-1] if k <= len(divs) else 0)
