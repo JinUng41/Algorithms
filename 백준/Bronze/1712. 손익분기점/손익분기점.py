2110255d7504dc01e684eecac642cbcce89f5575
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-02-26 11:16:28 +0900

    [Bronze II] Title: 손익분기점, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.py
+++ 백준/Bronze/1712. 손익분기점/손익분기점.py
@@ -1,7 +1,2 @@
 a, b, c = map(int, input().split())
-dividor = c-b
-if dividor == 0:
-    print(-1)
-else:
-    result = a//(c-b) + 1
-    print(result if result > 0 else -1)
+print(-1 if c <= b else a // (c - b) + 1)
