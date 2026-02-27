d8588f3660a790840b151fe203c5a77d5ac42b08
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-02-27 17:24:42 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -1,17 +1,12 @@
 a = input().strip()
 b = input().strip()
 
-countA = {}
-countB = {}
+count = {}
 
 for char in a:
-    countA[char] = countA.get(char, 0) + 1
+    count[char] = count.get(char, 0) + 1
 
 for char in b:
-    countB[char] = countB.get(char, 0) + 1
+    count[char] = count.get(char, 0) - 1
 
-answer = 0
-for char in set(countA) | set(countB):
-    answer += abs(countA.get(char, 0) - countB.get(char, 0))
-
-print(answer)
+print(sum(abs(value) for value in count.values()))
