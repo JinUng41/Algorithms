679ccecdb431af0525f020f9df4ce368a708d553
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-03 23:44:32 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,12 +1,15 @@
 import sys
 
-for _ in range(int(sys.stdin.readline())):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
-    apart = [[0] * (n+1) for _ in range(k+1)]
-    for i in range(1, n+1):
-        apart[0][i] = i
-    for i in range(1, k+1):
-        for j in range(1, n+1):
-            apart[i][j] = apart[i][j-1] + apart[i-1][j]
-    sys.stdout.write(str(apart[k][n]) + "\n")
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+
+    dp = [i for i in range(n + 1)]
+
+    for _ in range(k):
+        for j in range(1, n + 1):
+            dp[j] += dp[j - 1]
+
+    print(dp[n])
