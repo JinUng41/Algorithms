d34bb5e82936ccfc57ff86117d66337c8d46bffd
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-04 16:09:20 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -3,20 +3,14 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    coins = [0 for _ in range(4)]
+    coins = [25, 10, 5, 1]
+    count = [0 for _ in range(4)]
     cost = int(input())
-    while cost > 0:
-        if cost >= 25:
-            coins[0] += 1
-            cost -= 25
-        elif cost >= 10:
-            coins[1] += 1
-            cost -= 10
-        elif cost >= 5:
-            coins[2] += 1
-            cost -= 5
-        elif cost >= 1:
-            coins[3] += 1
-            cost -= 1
-    
-    sys.stdout.write(" ".join(map(str, coins)) + "\n")
+    for i in range(0, 4):
+        coin = coins[i]
+        if cost % coin >= 0:
+            div = cost // coin
+            count[i] = div
+            cost %= coin
+
+    sys.stdout.write(" ".join(map(str, count)) + "\n")
