3e9e8ca64f8fc96995c17e4d2bbc5b241d330b26
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-05 12:19:30 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+result = 0
+for i in range(0, n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            sum = cards[i] + cards[j] + cards[k]
+            if sum <= m and result < sum:
+                result = sum
+
+print(result)
