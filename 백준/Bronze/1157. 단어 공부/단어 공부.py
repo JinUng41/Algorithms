f6889239b80dba455e558005b6d6e3b1a0219b2a
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-06 18:12:00 +0900

    [Bronze I] Title: 단어 공부, Time: 200 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,6 +1,6 @@
-count_dict = {}
-for char in input().upper():
-    count_dict[char] = count_dict.get(char, 0) + 1
-max_count = max(count_dict.values())
-max_keys = [key for key, value in count_dict.items() if value == max_count]
-print(max_keys[0] if len(max_keys) == 1 else "?")
+s = input().upper()
+counts = [0] * 26
+for ch in s:
+    counts[ord(ch) - 65] += 1
+m = max(counts)
+print("?" if counts.count(m) > 1 else chr(counts.index(m) + 65))
