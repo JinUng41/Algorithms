961ab5cd732d4c029027eb87c834deb8f6f3e07c
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-07 11:43:12 +0900

    [Bronze II] Title: 막대기, Time: 72 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,16 +1,12 @@
 import sys
 
 input = sys.stdin.readline
-
 n = int(input())
-sticks = []
-for _ in range(n):
-    sticks.append(int(input()))
-stack = []
-maxH = 0
+sticks = [int(input()) for _ in range(n)]
+visible_count = 0
+max_height = 0
 for stick in reversed(sticks):
-    if stick > maxH:
-        stack.append(stick)
-        maxH = stick
-
-print(len(stack))
+    if stick > max_height:
+        visible_count += 1
+        max_height = stick
+sys.stdout.write(str(visible_count))
