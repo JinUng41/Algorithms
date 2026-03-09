5c6aaf7bf5cb06709f0f799f3018e7762d803edc
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-09 14:38:58 +0900

    [Silver IV] Title: 큐 2, Time: 1384 ms, Memory: 112580 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -3,19 +3,17 @@ import sys
 input = sys.stdin.readline
 
 q = deque()
-result = []
 for _ in range(int(input())):
     command = input().split()
     if command[0] == "push":
         q.append(int(command[1]))
     elif command[0] == "pop":
-        result.append(q.popleft() if q else -1)
+        sys.stdout.write(str(q.popleft() if q else -1) + "\n")
     elif command[0] == "size":
-        result.append(len(q))
+        sys.stdout.write(str(len(q)) + "\n")
     elif command[0] == "empty":
-        result.append(0 if q else 1)
+        sys.stdout.write(str(0 if q else 1) + "\n")
     elif command[0] == "front":
-        result.append(q[0] if q else -1)
+        sys.stdout.write(str(q[0] if q else -1) + "\n")
     elif command[0] == "back":
-        result.append(q[-1] if q else -1)
-sys.stdout.write("\n".join(map(str, result)) + "\n")
+        sys.stdout.write(str(q[-1] if q else -1) + "\n")
