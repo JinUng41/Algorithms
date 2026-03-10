913849600a2d2483d53f1bea87c4c8407f67c52b
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-10 09:09:45 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,8 +1,9 @@
 n = int(input())
-dp = [0, 1, 1]
-if n < 3:
+dp = [0 for _ in range(n+1)]
+dp[1] = 1
+if n < 2:
     print(dp[n])
 else:
-    for i in range(3, n+1):
-        dp.append(dp[i-1] + dp[i-2])
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
     print(dp[n])
