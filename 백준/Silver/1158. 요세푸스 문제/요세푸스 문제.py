6ae60a053055a01f583d67787cf03934dad22703
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-03-26 14:16:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,10 +1,10 @@
-from collections import deque
-
 n, k = map(int, input().split())
-q = deque(range(1, n + 1))
+people = list(range(1, n + 1))
 answer = []
-while q:
-    step = (k - 1) % len(q)
-    q.rotate(-step)
-    answer.append(str(q.popleft()))
-print('<' + ', '.join(answer) + '>')
+index = 0
+
+while people:
+    index = (index + k - 1) % len(people)
+    answer.append(str(people.pop(index)))
+
+print("<" + ", ".join(answer) + ">")
