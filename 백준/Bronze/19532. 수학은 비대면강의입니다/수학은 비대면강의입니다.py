936a50d2374c4f5910185ca16042d52c3c8674a6
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-04-09 15:00:16 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,10 +1,4 @@
 a, b, c, d, e, f = map(int, input().split())
-found = False
-for x in range(-999, 1000):
-    for y in range(-999, 1000):
-        if a * x + b * y == c and d * x + e * y == f:
-            print(f'{x} {y}')
-            found = True
-            break
-    if found:
-        break
+x = (c*e - b*f) // (a*e - b*d)
+y = (a*f - c*d) // (a*e - b*d)
+print(f'{x} {y}')
