155a278f935c16fde9d659e2937422f740a5cb8f
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-04-10 14:17:49 +0900

    [Bronze I] Title: FizzBuzz, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,16 +1,11 @@
-more = 0
-num = 0
+n = 0
 for i in range(1, 4):
-    text = input()
-    if text != 'FizzBuzz' and text != 'Fizz' and text != 'Buzz':
-        num = int(text)
-        more = 4 - i
-num += more
-if num % 5 == 0 and num % 3 == 0:
-    print('FizzBuzz')
-elif num % 3 == 0:
-    print('Fizz')
-elif num % 5 == 0:
-    print('Buzz')
-else:
-    print(num)
+    s = input().strip()
+    if s.isdigit():
+        n = int(s) + (4 - i)
+ans = ""
+if n % 3 == 0:
+    ans += "Fizz"
+if n % 5 == 0:
+    ans += "Buzz"
+print(ans or n)
