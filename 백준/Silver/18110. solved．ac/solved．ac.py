e5667a469d04ea921588fe21ae6fcdcfc8062bad
Kim JinUng <91656206+JinUng41@users.noreply.github.com>
2026-04-20 14:42:02 +0900

    [Silver IV] Title: solved.ac, Time: 124 ms, Memory: 37164 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -3,12 +3,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-values = [int(input()) for _ in range(n)]
-values.sort()
-
 if n == 0:
     print(0)
 else:
+    values = sorted(int(input()) for _ in range(n))
     cut = int(n * 0.15 + 0.5)
     trimmed = values[cut:n - cut]
     answer = int(sum(trimmed) / len(trimmed) + 0.5)
